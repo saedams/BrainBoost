3ab40b3690c360264dde961cf7c16efae0283fe5
saeda adam hassan <saedams.ad@hva.nl>
2026-05-13 13:19:30 +0200

    Test 12

--- app/main/routes.py
+++ app/main/routes.py
@@ -262,9 +262,6 @@ def save_oefenopgaven(score, total_answered, incorrect_answers):
         (score, total_answered, json.dumps(incorrect_answers, ensure_ascii=False))
     )
 
-
-
-
 @bp.route("/oefenen-opgaven")
 def oefenen_opgaven():
     """
